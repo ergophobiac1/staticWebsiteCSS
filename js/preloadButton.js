242690b3f7105b45d19aa48b3f859e394337c766
Parth <parth123x@gmail.com>
2025-02-14 01:18:10 +0530

    added start button to begin the music

---
+++ js/preloadButton.js
@@ -0,0 +1,5 @@
+document.getElementById('startButton').addEventListener('click', function() {
+    // Hide the preloader and display the content
+    document.getElementById('preloader').style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+  });
